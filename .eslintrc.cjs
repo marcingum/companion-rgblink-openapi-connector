948c8ff9cf2a90430671610e3174565a90f1b386
Marcin Gumułka <marcingum@gmail.com>
2024-08-18 01:16:19 +0200

    js and ts files

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	extends: './node_modules/@companion-module/tools/eslint/main.cjs',
+	// You can add overrides here
+	"prettier/prettier": [
+		"error",
+		{
+			"endOfLine": "auto"
+		}
+	],
+}
